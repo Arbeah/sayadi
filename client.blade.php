1e4283c710cb5d12724b7241752e97223d797ab6
Arbeah <151046403+Arbeah@users.noreply.github.com>
2024-12-30 09:29:54 +0800

    Add files via upload

---
+++ client.blade.php
@@ -0,0 +1,89 @@
+<section class="client_section layout_padding">
+         <div class="container">
+            <div class="heading_container heading_center">
+               <h2>
+                  Customer's Testimonial
+               </h2>
+            </div>
+            <div id="carouselExample3Controls" class="carousel slide" data-ride="carousel">
+               <div class="carousel-inner">
+                  <div class="carousel-item active">
+                     <div class="box col-lg-10 mx-auto">
+                        <div class="img_container">
+                           <div class="img-box">
+                              <div class="img_box-inner">
+                                 <img src="images/client2.jpg" alt="">
+                              </div>
+                           </div>
+                        </div>
+                        <div class="detail-box">
+                           <h5>
+                              Arbeah Sayadi
+                           </h5>
+                           <h6>
+                              Customer
+                           </h6>
+                           <p>
+                           <i>I absolutely love shopping here! The products are high-quality, and the customer service is outstanding. I’ll definitely recommend this to all my friends! </i>
+                           </p>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="carousel-item">
+                     <div class="box col-lg-10 mx-auto">
+                        <div class="img_container">
+                           <div class="img-box">
+                              <div class="img_box-inner">
+                                 <img src="images/jenn.jpg" alt="">
+                              </div>
+                           </div>
+                        </div>
+                        <div class="detail-box">
+                           <h5>
+                              Jennifer Lawrence
+                           </h5>
+                           <h6>
+                              Customer
+                           </h6>
+                           <p>
+                           <i>Shopping here has been a game-changer! The products are top-notch, delivery is always on time, and the customer service is unmatched. I’m so happy I found this store!</i>
+                           </p>
+                        </div>
+                     </div>
+                  </div>
+                  <div class="carousel-item">
+                     <div class="box col-lg-10 mx-auto">
+                        <div class="img_container">
+                           <div class="img-box">
+                              <div class="img_box-inner">
+                                 <img src="images/amy.jpg" alt="">
+                              </div>
+                           </div>
+                        </div>
+                        <div class="detail-box">
+                           <h5>
+                              Amy Adams
+                           </h5>
+                           <h6>
+                              Customer
+                           </h6>
+                           <p>
+                           <i>This store exceeded my expectations! Fast delivery, amazing deals, and the product quality is top-notch. I’ll definitely be a returning customer!</i>
+                           </p>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+               <div class="carousel_btn_box">
+                  <a class="carousel-control-prev" href="#carouselExample3Controls" role="button" data-slide="prev">
+                  <i class="fa fa-long-arrow-left" aria-hidden="true"></i>
+                  <span class="sr-only">Previous</span>
+                  </a>
+                  <a class="carousel-control-next" href="#carouselExample3Controls" role="button" data-slide="next">
+                  <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+                  <span class="sr-only">Next</span>
+                  </a>
+               </div>
+            </div>
+         </div>
+      </section>
